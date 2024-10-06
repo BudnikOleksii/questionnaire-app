06b433011d5628c802d560ca7bd0513a28d07829
Oleksii Budnik <budnikoleksii@gmail.com>
2024-10-06 11:06:11 +0300

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 
 import { Provider } from 'react-redux';
 import { wrapper } from '@/store/store';
+const test = 1;
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
